7922fff73d2273a4fbf3ab97c3d5653a5475b688
htutwaiphyoe <htutwaiphyoe@gmail.com>
2021-07-16 08:07:58 +0630

    #add: header js

--- js/main.js
+++ js/main.js
@@ -1,39 +1,47 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
-
+const navClose = document.getElementById("nav-close");
+const navToggle = document.getElementById("nav-toggle");
+const navMenu = document.getElementById("nav-menu");
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-
+if (navToggle) {
+    navToggle.addEventListener("click", () => {
+        navMenu.classList.add("nav-show");
+    });
+}
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-
+if (navClose) {
+    navClose.addEventListener("click", () => {
+        navMenu.classList.remove("nav-show");
+    });
+}
 
 /*==================== REMOVE MENU MOBILE ====================*/
+const navLinks = document.querySelectorAll(".nav__link");
 
+navLinks.forEach((navLink) =>
+    navLink.addEventListener("click", () => {
+        navMenu.classList.remove("nav-show");
+    })
+);
 
 /*==================== ACCORDION SKILLS ====================*/
 
-
 /*==================== QUALIFICATION TABS ====================*/
-
 
 /*==================== SERVICES MODAL ====================*/
 
-
 /*==================== PORTFOLIO SWIPER  ====================*/
-
 
 /*==================== TESTIMONIAL ====================*/
 
-
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
+/*==================== CHANGE BACKGROUND HEADER ====================*/
 
-/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+/*==================== SHOW SCROLL UP ====================*/
 
-
-/*==================== SHOW SCROLL UP ====================*/ 
-
-
-/*==================== DARK LIGHT THEME ====================*/ 
+/*==================== DARK LIGHT THEME ====================*/
