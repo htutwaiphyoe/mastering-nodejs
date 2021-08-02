4d45ed834fe1233feb62a7d0fcab89fb73a2de10
htutwaiphyoe <htutwaiphyoe@gmail.com>
2021-08-02 09:10:42 +0630

    #add: portfolio html, css and pagination with swpier

--- js/main.js
+++ js/main.js
@@ -80,6 +80,18 @@ modalCloses.forEach((modalClose, i) => {
 });
 
 /*==================== PORTFOLIO SWIPER  ====================*/
+let swiper = new Swiper(".portfolio__container", {
+    cssMode: true,
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
 
 /*==================== TESTIMONIAL ====================*/
 
