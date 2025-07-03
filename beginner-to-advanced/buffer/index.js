91f05234df2a5a1e25e9c030b9eda9c8875b3d43
hwp <70026748+htutwaiphyoe@users.noreply.github.com>
2025-07-03 14:46:05 +0700

    feat: node buffer

--- beginner-to-advanced/buffer/index.js
+++ beginner-to-advanced/buffer/index.js
@@ -4,8 +4,13 @@ const buffer = Buffer.alloc(4);
 
 console.log(buffer[0]);
 
-const nameBuffer = Buffer.from("Hello, Htut");
+const helloBuffer = Buffer.from("Hello, ");
+const nameBuffer = Buffer.from("Htut Wai Phyo");
 
 console.log(nameBuffer);
 console.log(nameBuffer.toString());
 console.log(nameBuffer.toString("utf-8", 0, 4));
+
+const greetingBuffer = Buffer.concat([helloBuffer, nameBuffer]);
+
+console.log(greetingBuffer.toString());
