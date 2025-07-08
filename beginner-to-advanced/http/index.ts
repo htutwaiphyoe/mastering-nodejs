dba8c010cfe7cb4af6d761ccf5e6074b5aa2d65f
hwp <70026748+htutwaiphyoe@users.noreply.github.com>
2025-07-08 20:14:38 +0700

    feat: 404 response

--- beginner-to-advanced/http/index.ts
+++ beginner-to-advanced/http/index.ts
@@ -2,33 +2,43 @@ import http from "node:http";
 
 const port = 8000;
 
+const url = {
+  root: "/",
+  contact: "/contact-us",
+};
+
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "content-type": "application/json" });
+  switch (req.url) {
+    case url.root:
+      res.writeHead(200, { "content-type": "application/json" });
 
-  if (req.url === "/") {
-    return res.end(
-      JSON.stringify({
-        statue: "success",
-        message: "Hello",
-      }),
-    );
+      return res.end(
+        JSON.stringify({
+          statue: "success",
+          message: "Hello",
+        }),
+      );
+
+    case url.contact:
+      res.writeHead(200, { "content-type": "application/json" });
+
+      return res.end(
+        JSON.stringify({
+          statue: "success",
+          message: "Contact Us",
+        }),
+      );
+
+    default:
+      res.writeHead(404, { "content-type": "application/json" });
+
+      res.end(
+        JSON.stringify({
+          statue: "fail",
+          message: "Page Not Found",
+        }),
+      );
   }
-
-  if (req.url === "/contact-us") {
-    return res.end(
-      JSON.stringify({
-        statue: "success",
-        message: "Contact Us",
-      }),
-    );
-  }
-
-  res.end(
-    JSON.stringify({
-      statue: "fail",
-      message: "Page Not Found",
-    }),
-  );
 });
 
 server.listen(port, () => {
