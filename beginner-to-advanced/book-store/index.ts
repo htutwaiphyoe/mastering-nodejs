d2e93667e4bda94cdca94da668196449c62ce3f6
hwp <70026748+htutwaiphyoe@users.noreply.github.com>
2025-07-16 22:05:05 +0700

    feat(book-store): body type

--- beginner-to-advanced/book-store/index.ts
+++ beginner-to-advanced/book-store/index.ts
@@ -3,6 +3,8 @@ import { books } from "./db";
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/books", (req, res) => {
   res.status(200).json({
     status: "success",
@@ -32,6 +34,23 @@ app.get("/books/:id", (req, res) => {
   res.status(200).json({
     status: "success",
     book,
+  });
+});
+
+app.post("/books", (req, res) => {
+  const body = req.body;
+
+  if (!body) {
+    return res.status(400).json({
+      status: "error",
+      message: "data is invalid.",
+    });
+  }
+
+  books.push({ id: books.length, ...body });
+
+  res.status(201).json({
+    status: "success",
   });
 });
 
