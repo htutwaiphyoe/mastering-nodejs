ab08a813cfa625838b5d5fb91a9baef2337c0351
hwp <70026748+htutwaiphyoe@users.noreply.github.com>
2025-07-22 20:29:07 +0700

    feat(book-store): express controller

---
+++ beginner-to-advanced/book-store/features/books/books.controller.ts
@@ -0,0 +1,66 @@
+import { Request, Response } from "express";
+import { books } from "../../db";
+
+export const getBooks = (req: Request, res: Response) => {
+  res.status(200).json({
+    status: "success",
+    books,
+  });
+};
+
+export const getBookById = (req: Request, res: Response) => {
+  const id = +req.params.id;
+
+  if (isNaN(id)) {
+    return res.status(400).json({
+      status: "error",
+      message: "Id is invalid.",
+    });
+  }
+
+  const book = books.find((book) => book.id === id);
+
+  if (!book) {
+    return res.status(404).json({
+      status: "failed",
+      message: "Book is not found.",
+    });
+  }
+
+  res.status(200).json({
+    status: "success",
+    book,
+  });
+};
+
+export const createBook = (req: Request, res: Response) => {
+  const { title, author } = req.body;
+
+  if (!title || !author) {
+    return res.status(400).json({
+      status: "error",
+      message: "data is invalid.",
+    });
+  }
+
+  const book = { id: books.length + 1, title, author };
+
+  books.push(book);
+
+  res.status(201).json({
+    status: "success",
+    book,
+  });
+};
+
+export const deleteBook = (req: Request, res: Response) => {
+  const id = +req.params.id;
+
+  const book = books.findIndex((book) => book.id === id);
+
+  books.splice(book, 1);
+
+  res.status(204).json({
+    status: "success",
+  });
+};
--- beginner-to-advanced/book-store/features/books/books.router.ts
+++ beginner-to-advanced/book-store/features/books/books.router.ts
@@ -1,71 +1,20 @@
 import express from "express";
-import { books } from "../../db";
 import { Book } from "../../types";
+import {
+  createBook,
+  deleteBook,
+  getBookById,
+  getBooks,
+} from "./books.controller";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.status(200).json({
-    status: "success",
-    books,
-  });
-});
+router.get("/", getBooks);
 
-router.get("/:id", (req, res) => {
-  const id = +req.params.id;
+router.get("/:id", getBookById);
 
-  if (isNaN(id)) {
-    return res.status(400).json({
-      status: "error",
-      message: "Id is invalid.",
-    });
-  }
+router.post<{}, unknown, Book>("/", createBook);
 
-  const book = books.find((book) => book.id === id);
-
-  if (!book) {
-    return res.status(404).json({
-      status: "failed",
-      message: "Book is not found.",
-    });
-  }
-
-  res.status(200).json({
-    status: "success",
-    book,
-  });
-});
-
-router.post<{}, unknown, Book>("/", (req, res) => {
-  const { title, author } = req.body;
-
-  if (!title || !author) {
-    return res.status(400).json({
-      status: "error",
-      message: "data is invalid.",
-    });
-  }
-
-  const book = { id: books.length + 1, title, author };
-
-  books.push(book);
-
-  res.status(201).json({
-    status: "success",
-    book,
-  });
-});
-
-router.delete("/:id", (req, res) => {
-  const id = +req.params.id;
-
-  const book = books.findIndex((book) => book.id === id);
-
-  books.splice(book, 1);
-
-  res.status(204).json({
-    status: "success",
-  });
-});
+router.delete("/:id", deleteBook);
 
 export default router;
