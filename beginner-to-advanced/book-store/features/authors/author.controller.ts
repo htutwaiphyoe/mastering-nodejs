70b64967b887f0c7030ac8a3ed59e96ece2ce52b
hwp <70026748+htutwaiphyoe@users.noreply.github.com>
2025-08-23 12:00:00 +0700

    feat(book-store): get author by id

--- beginner-to-advanced/book-store/features/authors/author.controller.ts
+++ beginner-to-advanced/book-store/features/authors/author.controller.ts
@@ -1,6 +1,9 @@
 import type { Request, Response } from "express";
 import db from "@/db";
 import { authorsTable } from "./author.model";
+import type { Uuid } from "@/lib/validators";
+import { ApiError } from "@/lib/api-error";
+import { eq } from "drizzle-orm";
 
 export const getAllAuthors = async (req: Request, res: Response) => {
   const authors = await db.select().from(authorsTable);
@@ -8,5 +11,27 @@ export const getAllAuthors = async (req: Request, res: Response) => {
   res.status(200).json({
     status: "success",
     authors,
+  });
+};
+
+export const getAuthorById = async (
+  req: Request<{ id: Uuid }>,
+  res: Response,
+) => {
+  const { id } = req.params;
+
+  const [author] = await db
+    .select()
+    .from(authorsTable)
+    .where(eq(authorsTable.id, id))
+    .limit(1);
+
+  if (!author) {
+    throw ApiError.notFound("Author is not found.");
+  }
+
+  res.status(200).json({
+    status: "success",
+    author,
   });
 };
--- beginner-to-advanced/book-store/features/authors/author.router.ts
+++ beginner-to-advanced/book-store/features/authors/author.router.ts
@@ -1,8 +1,12 @@
 import express from "express";
-import { getAllAuthors } from "./author.controller";
+import { validate } from "@/middleware/validate";
+import { idParamSchema } from "@/lib/validators";
+import { getAllAuthors, getAuthorById } from "./author.controller";
 
 const router = express.Router();
 
 router.get("/", getAllAuthors);
+
+router.get("/:id", validate("params", idParamSchema), getAuthorById);
 
 export default router;
